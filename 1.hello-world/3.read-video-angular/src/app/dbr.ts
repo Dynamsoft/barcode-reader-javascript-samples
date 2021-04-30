2ca9a186168f8f0a6b4b256d2d7712713f94cb26
Tom Kent <tom@dynamsoft.com>
2021-04-30 19:19:22 +0800

    0430

--- 1.hello-world/3.read-video-angular/src/app/dbr.ts
+++ 1.hello-world/3.read-video-angular/src/app/dbr.ts
@@ -1,6 +1,10 @@
 import DBR from "dynamsoft-javascript-barcode";
 DBR.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@8.2.3/dist/";
-// Please visit https://www.dynamsoft.com/customer/license/trialLicense to get a trial license
+/**
+ * Please note that the organizationID '200001' corresponds to Dynamsoft's public trial license which is reserved for public samples.
+ * When hosting the sample in your own environment, please obtain a private trial license or a full license by contacting the
+ * Dynamsoft Support Team via support@dynamsoft.com
+ */
 DBR.BarcodeReader.organizationID = "100448235";
 // DBR.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.
 export default DBR;
