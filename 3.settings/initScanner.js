c53d83b42bf42f9ba8695526f23ba3f6134e0d5c
Tom Kent <tom@dynamsoft.com>
2021-05-08 17:00:30 +0800

    settings sample almost ok, all settings sample has some bugs

--- 3.settings/initScanner.js
+++ 3.settings/initScanner.js
@@ -1,62 +1,85 @@
 /* The purpose of this file is to combine the DBR Scanner initialization code as well as the common operations among all the samples in the settings folder */
 
 let scanner = null;
+let initialSettings = null;
+let loadingText = document.getElementById('lib-load');
 let modalSettings = document.getElementById('settingsModal');
 let settingsBtn = document.getElementById('openSimpleSettingsMenu');
-let readBtn = document.getElementById('readBarcode');
 let closeModalBtn = document.getElementById('closeModalBtn');
+let resultBox = document.getElementById('result');
 
 // Create a BarcodeScanner instance on page load to speed things up.
 window.onload = async function () {
     try {
         Dynamsoft.DBR.BarcodeReader.organizationID = "100448235";
+        loadingText.hidden = false;
         scanner = scanner || await Dynamsoft.DBR.BarcodeScanner.createInstance();
+        initialSettings = await scanner.getRuntimeSettings();
         await scanner.setUIElement(document.getElementById('div-video-container'));
+        startReading();
     } catch (ex) {
         alert(ex.message);
         throw ex;
     }
-};
 
-// Open the settings modal menu once the settings button is clicked
-settingsBtn.onclick = function () {
-    modalSettings.style.display = "block";
-}
+    // Open the settings modal menu once the settings button is clicked
+    settingsBtn.onclick = function () {
+        modalSettings.style.display = "block";
+    }
 
-// Close the modal when the 'x' is clicked
-closeModalBtn.onclick = function () {
-    modalSettings.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modalSettings) {
+    // Close the modal when the 'x' is clicked
+    closeModalBtn.onclick = function () {
         modalSettings.style.display = "none";
     }
-}
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modalSettings) {
+            modalSettings.style.display = "none";
+        }
+    }
+
+    /* The following defines an ontouchstart event for all the tooltip elements. This trigger is being added to take into account that there is no 'hover' event when using a mobile browser. Therefore, this event will be added so that the tooltip appears whenever it is tapped on a mobile browser */
+    const tooltips = document.getElementsByClassName("tooltip");
+    for (let i = 0; i < tooltips.length; i++) {
+        tooltips[i].addEventListener('click', function () {
+            var _tip = this;
+            _tip.setAttribute('data-balloon-visible', '');
+            setTimeout(function () {
+                _tip.removeAttribute('data-balloon-visible');
+            }, 3000);
+        })
+    }
+    if (typeof initPage === "function")
+        initPage();
+};
 
 // open the scanner video and start decoding once the Read Barcode button is clicked
-readBtn.onclick = async function () {
+async function startReading() {
     try {
-        scanner.onUnduplicatedRead = (txt, result) => {
-            alert(txt);
-            console.log(result);
-        }
+        scanner.onFrameRead = (_results) => {
+            for (let result of _results) {
+                resultBox.value = result.barcodeFormatString + ": " + result.barcodeText;
+            }
+        };
         await scanner.show();
+        loadingText.hidden = true;
+        resultBox.style.visibility = "visible";
+        settingsBtn.style.visibility = "visible";
     } catch (ex) {
         alert(ex.message);
         throw ex;
     }
 }
 
-/* The following defines an ontouchstart event for all the tooltip elements. This trigger is being added to take into account that there is no 'hover' event when using a mobile browser. Therefore, this event will be added so that the tooltip appears whenever it is tapped on a mobile browser */
-const tooltips = document.getElementsByClassName("tooltip");
-for (let i = 0; i < tooltips.length; i++) {
-    tooltips[i].addEventListener('click', function () {
-        console.log(tooltips[i])
-        tooltips[i].setAttribute('data-balloon-visible', '');
-        setTimeout(function () {
-            console.log(tooltips[i])
-            tooltips[i].removeAttribute('data-balloon-visible');
-        }, 3000);
-    })
+function showModes(elID, modes, index) {
+    var modesToShow = [];
+    for (var i = 0; i < modes.length; i++) {
+        var m = modes[i];
+        if (i === index) {
+            modesToShow.push("<strong>" + m + "</strong>")
+        } else {
+            modesToShow.push(m)
+        }
+    }
+    document.getElementById(elID).innerHTML = modesToShow.join(", ");
 }
