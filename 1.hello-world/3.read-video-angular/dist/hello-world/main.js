a165b44b0e5faaa9c6dfe63d25460354ef7c32fd
Tom Kent <tom@dynamsoft.com>
2021-05-11 14:53:20 +0800

    adds license alert

--- 1.hello-world/3.read-video-angular/dist/hello-world/main.js
+++ 1.hello-world/3.read-video-angular/dist/hello-world/main.js
@@ -193,9 +193,21 @@ const environment = {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var dynamsoft_javascript_barcode__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! dynamsoft-javascript-barcode */ "bHga");
 
+/** LICENSE ALERT - README
+ * The library requires a license to work, the APIs organizationID and handshakeCode specify how to acquire a license.
+ * If nothing is specified, a 7-day (public) trial license will be used by default which is the case in this sample.
+ * Note that network connection is required for this license acquisition to work.
+ */
+/* When using your own license, please uncomment the following lines and fill in your own information. */
+/* For more information, please refer to https://www.dynamsoft.com/license-tracking/docs/about/licensefaq.html?ver=latest#how-to-use-a-trackable-license. */
+// DBR..BarcodeReader.organizationID = "YOUR-ORGANIZATION-ID";
+// DBR..BarcodeReader.handshakeCode = "A-SPECIFIC-HANDSHAKECODE";
+// DBR..BarcodeReader.sessionPassword = "PASSWORD-TO-PROTECT-YOUR-LICENSE"; // Important field to protect your license.
+// DBR..BarcodeReader.licenseServer = ["YOUR-OWN-MAIN-LTS", "YOUR-OWN-STANDBY-LTS"]; // Ignore this line if you are using Dynamsoft-hosting LTS
+/* The API "productKeys" is an alternative way to license the library, the major difference is that it does not require a network. Contact support@dynamsoft.com for more information. */
+// DBR..BarcodeReader.productKeys = "YOUR-PRODUCT-KEY";
+/** LICENSE ALERT - THE END */
 dynamsoft_javascript_barcode__WEBPACK_IMPORTED_MODULE_0__["default"].BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@8.2.3/dist/";
-// Please visit https://www.dynamsoft.com/customer/license/trialLicense to get a trial license
-dynamsoft_javascript_barcode__WEBPACK_IMPORTED_MODULE_0__["default"].BarcodeReader.organizationID = "100448235";
 // DBR.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.
 /* harmony default export */ __webpack_exports__["default"] = (dynamsoft_javascript_barcode__WEBPACK_IMPORTED_MODULE_0__["default"]);
 
--- 3.settings/initScanner.js
+++ 3.settings/initScanner.js
@@ -11,7 +11,26 @@ let resultBox = document.getElementById('result');
 // Create a BarcodeScanner instance on page load to speed things up.
 window.onload = async function () {
     try {
-        Dynamsoft.DBR.BarcodeReader.organizationID = "100448235";
+        /** LICENSE ALERT - README
+         * The library requires a license to work, the APIs organizationID and handshakeCode specify how to acquire a license.
+         * If nothing is specified, a 7-day (public) trial license will be used by default which is the case in this sample.
+         * Note that network connection is required for this license acquisition to work.
+         */
+
+        /* When using your own license, please uncomment the following lines and fill in your own information. */
+        /* For more information, please refer to https://www.dynamsoft.com/license-tracking/docs/about/licensefaq.html?ver=latest#how-to-use-a-trackable-license. */
+
+        // Dynamsoft.DBR.BarcodeReader.organizationID = "YOUR-ORGANIZATION-ID";
+        // Dynamsoft.DBR.BarcodeReader.handshakeCode = "A-SPECIFIC-HANDSHAKECODE";
+        // Dynamsoft.DBR.BarcodeReader.sessionPassword = "PASSWORD-TO-PROTECT-YOUR-LICENSE"; // Important field to protect your license.
+        // Dynamsoft.DBR.BarcodeReader.licenseServer = ["YOUR-OWN-MAIN-LTS", "YOUR-OWN-STANDBY-LTS"]; // Ignore this line if you are using Dynamsoft-hosting LTS
+
+        /* The API "productKeys" is an alternative way to license the library, the major difference is that it does not require a network. Contact support@dynamsoft.com for more information. */
+
+        // Dynamsoft.DBR.BarcodeReader.productKeys = "YOUR-PRODUCT-KEY";
+
+        /** LICENSE ALERT - THE END */
+
         loadingText.hidden = false;
         scanner = scanner || await Dynamsoft.DBR.BarcodeScanner.createInstance();
         initialSettings = await scanner.getRuntimeSettings();
