27612d17e6a7de6f347134ec1db0027b49b065a2
Tom Kent <tom@dynamsoft.com>
2021-05-21 16:54:20 +0800

    update to 8.2.5

--- 1.hello-world/3.read-video-angular/dist/hello-world/main.js
+++ 1.hello-world/3.read-video-angular/dist/hello-world/main.js
@@ -200,12 +200,12 @@ __webpack_require__.r(__webpack_exports__);
  */
 /* When using your own license, please uncomment the following lines and fill in your own information. */
 /* For more information, please refer to https://www.dynamsoft.com/license-tracking/docs/about/licensefaq.html?ver=latest#how-to-use-a-trackable-license. */
-// DBR..BarcodeReader.organizationID = "YOUR-ORGANIZATION-ID";
-// DBR..BarcodeReader.handshakeCode = "A-SPECIFIC-HANDSHAKECODE";
-// DBR..BarcodeReader.sessionPassword = "PASSWORD-TO-PROTECT-YOUR-LICENSE"; // Important field to protect your license.
-// DBR..BarcodeReader.licenseServer = ["YOUR-OWN-MAIN-LTS", "YOUR-OWN-STANDBY-LTS"]; // Ignore this line if you are using Dynamsoft-hosting LTS
+// DBR.BarcodeReader.organizationID = "YOUR-ORGANIZATION-ID";
+// DBR.BarcodeReader.handshakeCode = "A-SPECIFIC-HANDSHAKECODE";
+// DBR.BarcodeReader.sessionPassword = "PASSWORD-TO-PROTECT-YOUR-LICENSE"; // Important field to protect your license.
+// DBR.BarcodeReader.licenseServer = ["YOUR-OWN-MAIN-LTS", "YOUR-OWN-STANDBY-LTS"]; // Ignore this line if you are using Dynamsoft-hosting LTS
 /* The API "productKeys" is an alternative way to license the library, the major difference is that it does not require a network. Contact support@dynamsoft.com for more information. */
-// DBR..BarcodeReader.productKeys = "YOUR-PRODUCT-KEY";
+// DBR.BarcodeReader.productKeys = "YOUR-PRODUCT-KEY";
 /** LICENSE ALERT - THE END */
 dynamsoft_javascript_barcode__WEBPACK_IMPORTED_MODULE_0__["default"].BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@8.2.3/dist/";
 // DBR.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.
