09a6ea8bbb6805d7a8d86b98dd46094d58785ae2
Cube <877211593@qq.com>
2022-06-16 15:02:24 +0800

    fix 'Failed to connect to Dynamsoft...' always shows when any error throws

--- 3.settings/initScanner.js
+++ 3.settings/initScanner.js
@@ -31,12 +31,14 @@ window.onload = async function () {
         await scanner.setUIElement(document.getElementById('div-ui-container'));
         startReading();
     } catch (ex) {
-        if (ex.message.indexOf("network connection error")) {
-            let customMsg = "Failed to connect to Dynamsoft License Server: network connection error. Check your Internet connection or contact Dynamsoft Support (support@dynamsoft.com) to acquire an offline license.";
-            console.log(customMsg);
-            alert(customMsg);
+        let errMsg;
+        if (ex.message.includes("network connection error")) {
+            errMsg = "Failed to connect to Dynamsoft License Server: network connection error. Check your Internet connection or contact Dynamsoft Support (support@dynamsoft.com) to acquire an offline license.";
+        } else {
+            errMsg = ex.message||ex;
         }
-        throw ex;
+        console.error(errMsg);
+        alert(errMsg);
     }
 
     // Open the settings modal menu once the settings button is clicked
