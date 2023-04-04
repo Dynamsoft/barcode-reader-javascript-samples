1bd393090edb8e227fc90a3b2d37d3dd7c42b0e8
Keillion <735699921@qq.com>
2023-04-04 17:16:28 +0800

    test pure js

--- 1.hello-world/14.read-video-webview/android/app/src/main/java/com/dynamsoft/javascript/readvideowebview/MainActivity.java
+++ 1.hello-world/14.read-video-webview/android/app/src/main/java/com/dynamsoft/javascript/readvideowebview/MainActivity.java
@@ -11,37 +11,36 @@ import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.Toast;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+    static int Camera_Permission_Request_Code = 32765;
     WebView mWebView;
-    DBRWebViewHelper dbrWebViewHelper;
-    Button mButton;
 
-    @SuppressLint("ShowToast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButton = findViewById(R.id.myButton);
-
-        // In order to avoid conflicts, you can use the following code to set the request code for requesting camera permission before you pollute
-        // dbrWebViewHelper.setCameraPermissionRequestCode(Your code);
 
         // Initialize WebView
         mWebView = findViewById(R.id.myWebview);
-        // Pollute your WebView
-        dbrWebViewHelper = new DBRWebViewHelper();
-        dbrWebViewHelper.pollute(mWebView);
-        Toast toast = Toast.makeText(this, "Loading Library...", Toast.LENGTH_LONG);
-        toast.show();
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        
         // for development, enabled debugging and clear html files cache
         WebView.setWebContentsDebuggingEnabled(true);
-        mWebView.clearCache(true);
         mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onPermissionRequest(final PermissionRequest request) {
+                if (ContextCompat.checkSelfPermission(mActivity, "android.permission.CAMERA") != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(mActivity, new String[]{"android.permission.CAMERA"}, Camera_Permission_Request_Code);
+                }
+                // else {
+                //     evaluateJavascript("startScanner()");
+                // }
                 request.grant(request.getResources());
             }
         });
@@ -50,29 +49,10 @@ public class MainActivity extends AppCompatActivity {
         mWebView.loadUrl("file:///android_asset/index.html");
     }
 
-    public void switchScanner(View view) {
-        CharSequence text = mButton.getText();
-        if (text.toString().equals("Start")) {
-            mButton.setText(R.string.btn_stop);
-            dbrWebViewHelper.startScanner();
-        } else {
-            mButton.setText(R.string.btn_start);
-            dbrWebViewHelper.stopScanner();
-        }
-    }
+    // @Override
+    // public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    //     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    //     //dbrWebViewHelper.cameraPermissionHandler(requestCode, permissions, grantResults);
 
-    public void showResult(String result) {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("Result:").setPositiveButton("OK", null).setMessage(result).show();
-    }
-
-    public void showController() {
-        runOnUiThread(() -> mButton.setVisibility(View.VISIBLE));
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        dbrWebViewHelper.cameraPermissionHandler(requestCode, permissions, grantResults);
-    }
+    // }
 }
