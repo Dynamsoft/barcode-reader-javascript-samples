e624ce7dafdb1fd6ac0e2b54be3bd96f8db433d0
Cube <877211593@qq.com>
2023-10-23 10:48:19 +0800

    add filter to hello-world.html

--- hello-world/angular/src/cvr.ts
+++ hello-world/angular/src/cvr.ts
@@ -4,7 +4,7 @@ import {
 } from '@dynamsoft/dynamsoft-capture-vision-router';
 
 CaptureVisionRouter.engineResourcePath =
-  'https://npm.scannerproxy.com/cdn/@dynamsoft/dynamsoft-capture-vision-router@2.0.20-dev-20231023101425/dist/';
+  'https://npm.scannerproxy.com/cdn/@dynamsoft/dynamsoft-capture-vision-router@2.0.20-dev-20231023103654/dist/';
 
 /** LICENSE ALERT - README
  * To use the library, you need to first specify a license key using the API "initLicense" as shown below.
