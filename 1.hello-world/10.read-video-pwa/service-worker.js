c0a93a695eb6920753ef78ce9850b47725efaaaa
Cube <877211593@qq.com>
2023-10-31 18:00:08 +0800

    Update service-worker.js

--- 1.hello-world/10.read-video-pwa/service-worker.js
+++ 1.hello-world/10.read-video-pwa/service-worker.js
@@ -1,5 +1,5 @@
-const dbrVersion = "9.6.31";
-const dbrCdn = `https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@${dbrVersion}/dist/`;
+const dbrCdn = `https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@9.6.31/dist/`;
+const dbrVersion = dbrCdn.slice(dbrCdn.search(/(?<=dynamsoft-javascript-barcode@)./), dbrCdn.search(/\/dist/));
 
 // Files to cache
 const cacheName = 'helloworld-pwa';
