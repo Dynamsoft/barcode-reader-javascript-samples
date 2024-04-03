0f1bf58949e714b33da62434de931fb602e6d9d3
SHEN <1014628363@qq.com>
2024-04-03 09:58:42 +0800

    fix bug

---
+++ use-case/dcp/types/utils/index.d.ts
@@ -0,0 +1,5 @@
+import CodeParser from "../class/CodeParser";
+import { ParsedResultItem } from "../interface/ParsedResultItem";
+export declare function handleParsedResultItem(item: ParsedResultItem): void;
+export declare function stringToUint8Array(str: string): number[];
+export declare function checkIsDisposed(parser: CodeParser): void;
