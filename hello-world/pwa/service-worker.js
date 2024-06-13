65174196dec3b7742487300c405e467635e3739f
Keillion <keillionv@gmail.com>
2024-06-13 19:19:29 +0800

    Update service-worker.js

--- hello-world/pwa/service-worker.js
+++ hello-world/pwa/service-worker.js
@@ -1,48 +1,36 @@
-const engineResourcePaths = {
-  std: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-std@1.2.10/dist/",
-  dip: "https://cdn.jsdelivr.net/npm/dynamsoft-image-processing@2.2.30/dist/",
-  core: "https://cdn.jsdelivr.net/npm/dynamsoft-core@3.2.30/dist/",
-  license: "https://cdn.jsdelivr.net/npm/dynamsoft-license@3.2.21/dist/",
-  cvr: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-router@2.2.30/dist/",
-  dbr: "https://cdn.jsdelivr.net/npm/dynamsoft-barcode-reader@10.2.10/dist/",
-  dce: "https://cdn.jsdelivr.net/npm/dynamsoft-camera-enhancer@4.0.3/dist/"
-};
+const cacheName = "dbrjs-helloworld-pwa";
 
-// Files to cache
-const cacheName = "helloworld-pwa";
-const appShellFiles = [
+const cdn = "https://cdn.jsdelivr.net/npm/dynamsoft-";
+// you can get file list from `F12 ==> console ==> network`,
+// and choose what you want to cache
+const filesShouldCache = [
   "./helloworld-pwa.html",
   "./dynamsoft-192x192.png",
   "./dynamsoft-512x512.png",
   "./helloworld-pwa.json",
-  `${engineResourcePaths.std}std.js`,
-  `${engineResourcePaths.std}std.wasm`,
-  `${engineResourcePaths.dip}dip.wasm`,
-  `${engineResourcePaths.core}core.js`,
-  `${engineResourcePaths.core}core.worker.js`,
-  `${engineResourcePaths.core}core.wasm`,
-  `${engineResourcePaths.license}license.js`,
-  `${engineResourcePaths.license}dls.license.dialog.html`,
-  `${engineResourcePaths.license}license.wasm`,
-  `${engineResourcePaths.utility}utility.js`,
-  `${engineResourcePaths.dbr}dbr.js`,
-  `${engineResourcePaths.dbr}dbr.wasm`,
-  `${engineResourcePaths.dbr}DBR-PresetTemplates.json`,
-  `${engineResourcePaths.cvr}cvr.js`,
-  `${engineResourcePaths.cvr}cvr.wasm`,
-  `${engineResourcePaths.cvr}cvr.worker.js`,
-  `${engineResourcePaths.dce}dce.js`,
-  `${engineResourcePaths.dce}dce.ui.html`,
+  `${cdn}barcode-reader-bundle@10.2.1000/dist/dbr.bundle.js`,
+  `${cdn}core@3.2.30/dist/core.worker.js`,
+  `${cdn}camera-enhancer@4.0.3/dist/dce.ui.html`,
+  `${cdn}capture-vision-std@1.2.10/dist/std.js`,
+  `${cdn}capture-vision-std@1.2.10/dist/std.wasm`,
+  `${cdn}core@3.2.30/dist/core.wasm`,
+  `${cdn}license@3.2.21/dist/license.wasm`,
+  `${cdn}image-processing@2.2.30/dist/dip.wasm`,
+  `${cdn}barcode-reader@10.2.10/dist/dbr.wasm`,
+  `${cdn}license@3.2.21/dist/dls.license.dialog.html`,
+  `${cdn}capture-vision-router@2.2.30/dist/cvr.worker.js`,
+  `${cdn}capture-vision-router@2.2.30/dist/cvr.wasm`,
+  `${cdn}barcode-reader@10.2.10/dist/DBR-PresetTemplates.json`,
 ];
 
 // Installing Service Worker
 self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
+  console.log("[Service Worker] installation started");
   e.waitUntil(
     (async () => {
       const cache = await caches.open(cacheName);
-      console.log("[Service Worker] Caching all: app shell and content");
-      await cache.addAll(appShellFiles);
+      await cache.addAll(filesShouldCache);
+      console.log("[Service Worker] Installation completed");
     })()
   );
 });
@@ -51,14 +39,14 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
       const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
-      }
+      if (r) { return r; }
       const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      if (e.request.method !== "POST") cache.put(e.request, response.clone());
+      // ** You can control whether you want to cache a resource
+      // ** e.g.
+      // if(e.request.url.startsWith('xxx')){
+      //   const cache = await caches.open(cacheName);
+      //   cache.put(e.request, response.clone());
+      // }
       return response;
     })()
   );
