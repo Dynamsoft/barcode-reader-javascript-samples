e3a395b9bab2b2083a5330e45e7fc99680ec4bdc
felixindynamsoft <felix.indrawan@dynamsoft.com>
2024-06-26 04:23:45 -0700

    feat: update angular sample and docs (#182)
    * feat: update angular sample and docs
    
    * fix: formatting
    
    * add file names
    
    ---------
    
    Co-authored-by: felixindrawan <indrawan.felix123@gmail.com>

--- hello-world/angular/src/app/dynamsoft.config.ts
+++ hello-world/angular/src/app/dynamsoft.config.ts
@@ -1,12 +1,23 @@
-import { CoreModule } from "dynamsoft-core";
-import { LicenseManager } from "dynamsoft-license";
-import "dynamsoft-barcode-reader";
+import { CoreModule } from 'dynamsoft-core';
+import { LicenseManager } from 'dynamsoft-license';
+import 'dynamsoft-barcode-reader';
+
+// Configures the paths where the .wasm files and other necessary resources for modules are located.
+CoreModule.engineResourcePaths = {
+  std: 'https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-std@1.2.10/dist/',
+  dip: 'https://cdn.jsdelivr.net/npm/dynamsoft-image-processing@2.2.30/dist/',
+  core: 'https://cdn.jsdelivr.net/npm/dynamsoft-core@3.2.30/dist/',
+  license: 'https://cdn.jsdelivr.net/npm/dynamsoft-license@3.2.21/dist/',
+  cvr: 'https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-router@2.2.30/dist/',
+  dbr: 'https://cdn.jsdelivr.net/npm/dynamsoft-barcode-reader@10.2.10/dist/',
+  dce: 'https://cdn.jsdelivr.net/npm/dynamsoft-camera-enhancer@4.0.3/dist/',
+};
 
 /** LICENSE ALERT - README
  * To use the library, you need to first specify a license key using the API "initLicense()" as shown below.
  */
 
-LicenseManager.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9");
+LicenseManager.initLicense('DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9', true);
 
 /**
  * You can visit https://www.dynamsoft.com/customer/license/trialLicense?utm_source=github&product=dbr&package=js to get your own trial license good for 30 days.
@@ -15,15 +26,5 @@ LicenseManager.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9");
  * LICENSE ALERT - THE END
  */
 
-CoreModule.engineResourcePaths = {
-  std: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-std@1.2.10/dist/",
-  dip: "https://cdn.jsdelivr.net/npm/dynamsoft-image-processing@2.2.30/dist/",
-  core: "https://cdn.jsdelivr.net/npm/dynamsoft-core@3.2.30/dist/",
-  license: "https://cdn.jsdelivr.net/npm/dynamsoft-license@3.2.21/dist/",
-  cvr: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-router@2.2.30/dist/",
-  dbr: "https://cdn.jsdelivr.net/npm/dynamsoft-barcode-reader@10.2.10/dist/",
-  dce: "https://cdn.jsdelivr.net/npm/dynamsoft-camera-enhancer@4.0.3/dist/"
-};
-
 // Optional. Preload "BarcodeReader" module for reading barcodes. It will save time on the initial decoding by skipping the module loading.
-CoreModule.loadWasm(["DBR"]);
+CoreModule.loadWasm(['DBR']);
--- hello-world/angular/src/app/image-capture/image-capture.component.ts
+++ hello-world/angular/src/app/image-capture/image-capture.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import "../dynamsoft.config";
-import { EnumCapturedResultItemType } from "dynamsoft-core";
-import type { BarcodeResultItem } from "dynamsoft-barcode-reader";
-import { CaptureVisionRouter } from "dynamsoft-capture-vision-router";
+import '../dynamsoft.config';
+import { EnumCapturedResultItemType } from 'dynamsoft-core';
+import type { BarcodeResultItem } from 'dynamsoft-barcode-reader';
+import { CaptureVisionRouter } from 'dynamsoft-capture-vision-router';
 
 @Component({
   selector: 'app-image-capture',
@@ -11,49 +11,57 @@ import { CaptureVisionRouter } from "dynamsoft-capture-vision-router";
   standalone: true,
 })
 export class ImageCaptureComponent {
-
-  @ViewChild('resDiv') resDiv?: ElementRef<HTMLDivElement>;
+  @ViewChild('results') resultsContainer?: ElementRef<HTMLDivElement>;
 
   pCvRouter?: Promise<CaptureVisionRouter>;
-  bDestoried = false;
+  isDestroyed = false;
 
   captureImage = async (e: Event) => {
-    let files = [...(e.target! as HTMLInputElement).files as any as File[]];
-    (e.target! as HTMLInputElement).value = '';
-    this.resDiv!.nativeElement.innerText = "";
+    let files = [...((e.target! as HTMLInputElement).files as any as File[])];
+    (e.target! as HTMLInputElement).value = ''; // reset input
+    this.resultsContainer!.nativeElement.innerText = '';
     try {
-      const cvRouter = await (this.pCvRouter = this.pCvRouter || CaptureVisionRouter.createInstance());
-      if (this.bDestoried) return;
-      
-      for(let file of files){
+      // ensure cvRouter is created only once
+      const cvRouter = await (this.pCvRouter =
+        this.pCvRouter || CaptureVisionRouter.createInstance());
+      if (this.isDestroyed) return;
+
+      for (let file of files) {
         // Decode selected image with 'ReadBarcodes_SpeedFirst' template.
-        const result = await cvRouter.capture(file, "ReadBarcodes_SpeedFirst");
-        if (this.bDestoried) return;
-  
-        if(files.length > 1){
-          this.resDiv!.nativeElement.innerText += `\n${file.name}:\n`;
+        const result = await cvRouter.capture(file, 'ReadBarcodes_SpeedFirst');
+        if (this.isDestroyed) return;
+
+        // Print file name if there's multiple files
+        if (files.length > 1) {
+          this.resultsContainer!.nativeElement.innerText += `\n${file.name}:\n`;
         }
         for (let _item of result.items) {
-          if(_item.type !== EnumCapturedResultItemType.CRIT_BARCODE) { continue; }
+          if (_item.type !== EnumCapturedResultItemType.CRIT_BARCODE) {
+            continue; // check if captured result item is a barcode
+          }
           let item = _item as BarcodeResultItem;
-          this.resDiv!.nativeElement.innerText += item.text + "\n";
+          this.resultsContainer!.nativeElement.innerText += item.text + '\n'; // output the decoded barcode text
           console.log(item.text);
         }
-        if (!result.items.length) this.resDiv!.nativeElement.innerText += 'No barcode found\n';
+        // If no items are found, display that no barcode was detected
+        if (!result.items.length)
+          this.resultsContainer!.nativeElement.innerText +=
+            'No barcode found\n';
       }
     } catch (ex: any) {
       let errMsg = ex.message || ex;
       console.error(errMsg);
       alert(errMsg);
     }
-  }
+  };
 
+  // dispose cvRouter when it's no longer needed
   async ngOnDestroy() {
-    this.bDestoried = true;
-    if(this.pCvRouter){
-      try{
+    this.isDestroyed = true;
+    if (this.pCvRouter) {
+      try {
         (await this.pCvRouter).dispose();
-      }catch(_){}
+      } catch (_) {}
     }
   }
 }
