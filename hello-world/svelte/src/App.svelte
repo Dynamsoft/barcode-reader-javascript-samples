6c55bbabb33f6e0eb4f958569708392c08a84f99
felixindynamsoft <felix.indrawan@dynamsoft.com>
2024-07-01 20:03:56 -0700

    feat: update svelte sample and docs (#184)
    * feat: update svelte sample and docs
    
    * fix: add white-space prewrap
    
    * feat: remove README generated by framework
    
    ---------
    
    Co-authored-by: felixindrawan <indrawan.felix123@gmail.com>
    Co-authored-by: Keillion <keillionv@gmail.com>

--- hello-world/svelte/src/App.svelte
+++ hello-world/svelte/src/App.svelte
@@ -9,24 +9,20 @@
   <div class="App">
     <div class="title">
       <h2 class="title-text">Hello World for Svelte</h2>
-      <img
-        class="title-logo"
-        src="https://www.svelte.cn/svelte-logo-horizontal.svg"
-        alt="svelte"
-      />
+      <img class="title-logo" src="https://www.svelte.cn/svelte-logo-horizontal.svg" alt="svelte" />
     </div>
-    <div class="top-btns">
+    <div class="buttons-container">
       <button
         style="background-color: {mode === 'video' ? 'rgb(255, 174, 55)' : 'white'}"
         on:click={() => {
           mode = "video";
-        }}>Video Capture</button
+        }}>Decode Video</button
       >
       <button
         style="background-color: {mode === 'image' ? 'rgb(255, 174, 55)' : 'white'}"
         on:click={() => {
           mode = "image";
-        }}>Image Capture</button
+        }}>Decode Image</button
       >
     </div>
     {#if mode === "video"}
@@ -51,40 +47,41 @@
     align-items: center;
     margin-top: 20px;
   }
+
   .title .title-logo {
     width: 70px;
     height: 30px;
     margin-left: 10px;
   }
 
-  .top-btns {
+  .buttons-container {
     width: 30%;
     margin: 20px auto;
   }
 
-  .top-btns button {
+  .buttons-container button {
     display: inline-block;
     border: 1px solid black;
     padding: 5px 15px;
     background-color: transparent;
     cursor: pointer;
-    /* TODO */
-    margin-right: -5px; 
+    margin-right: -5px;
   }
 
-  .top-btns button:first-child {
+  .buttons-container button:first-child {
     border-top-left-radius: 10px;
     border-bottom-left-radius: 10px;
     border-right: transparent;
   }
-  .top-btns button:nth-child(2) {
+
+  .buttons-container button:nth-child(2) {
     border-top-right-radius: 10px;
     border-bottom-right-radius: 10px;
     border-left: transparent;
   }
 
-  @media screen and (max-width: 500px) {
-    .top-btns {
+  @media screen and (max-width: 800px) {
+    .buttons-container {
       width: 70%;
     }
   }
