1d5da4a60160d2ea2c302b3558e8e432b1379503
felixindynamsoft <felix.indrawan@dynamsoft.com>
2024-07-01 20:03:56 -0700

    feat: update svelte sample and docs (#184)
    * feat: update svelte sample and docs
    
    * fix: add white-space prewrap
    
    * feat: remove README generated by framework
    
    ---------
    
    Co-authored-by: felixindrawan <indrawan.felix123@gmail.com>
    Co-authored-by: Keillion <keillionv@gmail.com>

--- hello-world/svelte/src/components/ImageCapture.svelte
+++ hello-world/svelte/src/components/ImageCapture.svelte
@@ -1,69 +1,73 @@
 <script lang="ts">
-import { onMount } from "svelte";
-import "../dynamsoft.config";
-import { EnumCapturedResultItemType } from "dynamsoft-core";
-import { type BarcodeResultItem } from "dynamsoft-barcode-reader";
-import { CaptureVisionRouter } from "dynamsoft-capture-vision-router";
+  import { onMount } from "svelte";
+  import "../dynamsoft.config";
+  import { EnumCapturedResultItemType } from "dynamsoft-core";
+  import { type BarcodeResultItem } from "dynamsoft-barcode-reader";
+  import { CaptureVisionRouter } from "dynamsoft-capture-vision-router";
 
-let resDiv: HTMLDivElement;
+  let resultsContainer: HTMLDivElement;
 
-let pCvRouter: Promise<CaptureVisionRouter>;
-let bDestoried = false;
+  let pCvRouter: Promise<CaptureVisionRouter>;
+  let isDestroyed = false;
 
+  const captureImage = async (e: Event) => {
+    let files = [...(e.target! as HTMLInputElement).files!];
+    (e.target! as HTMLInputElement).value = ""; // reset input
+    resultsContainer.innerText = "";
 
-const captureImage = async (e: Event) => {
-  let files = [...(e.target! as HTMLInputElement).files!];
-  (e.target! as HTMLInputElement).value = '';
-  resDiv.innerText = "";
-  try {
-    const cvRouter = await (pCvRouter = pCvRouter || CaptureVisionRouter.createInstance());
-    if (bDestoried) return;
-    
-    for(let file of files){
-      // Decode selected image with 'ReadBarcodes_SpeedFirst' template.
-      const result = await cvRouter.capture(file, "ReadBarcodes_SpeedFirst");
-      if (bDestoried) return;
+    try {
+      const cvRouter = await (pCvRouter = pCvRouter || CaptureVisionRouter.createInstance());
+      if (isDestroyed) return;
 
-      if(files.length > 1){
-        resDiv.innerText += `\n${file.name}:\n`;
+      for (let file of files) {
+        // Decode selected image with 'ReadBarcodes_SpeedFirst' template.
+        const result = await cvRouter.capture(file, "ReadBarcodes_SpeedFirst");
+        if (isDestroyed) return;
+
+        // Print file name if there's multiple files
+        if (files.length > 1) {
+          resultsContainer.innerText += `\n${file.name}:\n`;
+        }
+        for (let _item of result.items) {
+          if (_item.type !== EnumCapturedResultItemType.CRIT_BARCODE) {
+            continue; // check if captured result item is a barcode
+          }
+          let item = _item as BarcodeResultItem;
+          resultsContainer.innerText += item.text + "\n"; // output the decoded barcode text
+          console.log(item.text);
+        }
+        // If no items are found, display that no barcode was detected
+        if (!result.items.length) resultsContainer.innerText += "No barcode found\n";
       }
-      for (let _item of result.items) {
-        if(_item.type !== EnumCapturedResultItemType.CRIT_BARCODE) { continue; }
-        let item = _item as BarcodeResultItem;
-        resDiv.innerText += item.text + "\n";
-        console.log(item.text);
-      }
-      if (!result.items.length) resDiv.innerText += 'No barcode found\n';
-    }
-  } catch (ex: any) {
-    let errMsg = ex.message || ex;
-    console.error(errMsg);
-    alert(errMsg);
-  }
-};
-
-onMount(() => {
-  // onBeforeUnmount
-  return async()=>{
-    bDestoried = true;
-    if(pCvRouter){
-      try{
-        (await pCvRouter).dispose();
-      }catch(_){}
+    } catch (ex: any) {
+      let errMsg = ex.message || ex;
+      console.error(errMsg);
+      alert(errMsg);
     }
   };
-});
+
+  onMount(() => {
+    // onBeforeUnmount. dispose cvRouter when it's no longer needed
+    return async () => {
+      isDestroyed = true;
+      if (pCvRouter) {
+        try {
+          (await pCvRouter).dispose();
+        } catch (_) {}
+      }
+    };
+  });
 </script>
 
-<div class="capture-img">
-  <div class="img-ipt">
-    <input type="file" multiple on:change={captureImage} accept=".jpg,.jpeg,.icon,.gif,.svg,.webp,.png,.bmp"/>
+<div class="image-capture-container">
+  <div class="input-container">
+    <input type="file" multiple on:change={captureImage} accept=".jpg,.jpeg,.icon,.gif,.svg,.webp,.png,.bmp" />
   </div>
-  <div class="result-area" bind:this={resDiv}></div>
+  <div class="result" bind:this={resultsContainer}></div>
 </div>
 
 <style>
-  .capture-img {
+  .image-capture-container {
     width: 100%;
     height: 100%;
     font-family:
@@ -77,7 +81,7 @@ onMount(() => {
       monospace;
   }
 
-  .capture-img .img-ipt {
+  .image-capture-container .input-container {
     width: 80%;
     height: 100%;
     display: flex;
@@ -86,7 +90,7 @@ onMount(() => {
     margin: 0 auto;
   }
 
-  .capture-img .result-area {
+  .image-capture-container .result {
     margin-top: 20px;
   }
 </style>
