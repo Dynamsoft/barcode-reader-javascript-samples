cf2024c496bcfcb5f8a6f088a9785b5c0097f506
felixindynamsoft <felixindynamsoft@users.noreply.github.com>
2024-10-08 20:53:17 +0000

    Deploy preview for PR 214 🛫

---
+++ pr-preview/pr-214/hello-world/angular/src/app/dynamsoft.config.ts
@@ -0,0 +1,30 @@
+import { CoreModule } from 'dynamsoft-core';
+import { LicenseManager } from 'dynamsoft-license';
+import 'dynamsoft-barcode-reader';
+
+// Configures the paths where the .wasm files and other necessary resources for modules are located.
+CoreModule.engineResourcePaths = {
+  std: 'https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-std@1.2.10/dist/',
+  dip: 'https://cdn.jsdelivr.net/npm/dynamsoft-image-processing@2.2.30/dist/',
+  core: 'https://cdn.jsdelivr.net/npm/dynamsoft-core@3.2.30/dist/',
+  license: 'https://cdn.jsdelivr.net/npm/dynamsoft-license@3.2.21/dist/',
+  cvr: 'https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-router@2.2.30/dist/',
+  dbr: 'https://cdn.jsdelivr.net/npm/dynamsoft-barcode-reader@10.2.10/dist/',
+  dce: 'https://cdn.jsdelivr.net/npm/dynamsoft-camera-enhancer@4.0.3/dist/',
+};
+
+/** LICENSE ALERT - README
+ * To use the library, you need to first specify a license key using the API "initLicense()" as shown below.
+ */
+
+LicenseManager.initLicense('DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9', true);
+
+/**
+ * You can visit https://www.dynamsoft.com/customer/license/trialLicense?utm_source=samples&product=dbr&package=js to get your own trial license good for 30 days.
+ * Note that if you downloaded this sample from Dynamsoft while logged in, the above license key may already be your own 30-day trial license.
+ * For more information, see https://www.dynamsoft.com/barcode-reader/programming/javascript/user-guide/?ver=10.2.10&utm_source=samples#specify-the-license or contact support@dynamsoft.com.
+ * LICENSE ALERT - THE END
+ */
+
+// Optional. Preload "BarcodeReader" module for reading barcodes. It will save time on the initial decoding by skipping the module loading.
+CoreModule.loadWasm(['DBR']);
---
+++ pr-preview/pr-214/hello-world/svelte/src/components/VideoCapture.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import "../dynamsoft.config"; // import side effects. The license, engineResourcePath, so on.
+  import { CameraEnhancer, CameraView } from "dynamsoft-camera-enhancer";
+  import { CaptureVisionRouter } from "dynamsoft-capture-vision-router";
+  import { MultiFrameResultCrossFilter } from "dynamsoft-utility";
+
+  const componentDestroyedErrorMsg = "VideoCapture Component Destroyed";
+
+  let cameraViewContainer: HTMLDivElement;
+  let resultsContainer: HTMLDivElement;
+
+  let resolveInit: () => void;
+  const pInit: Promise<void> = new Promise((r) => {
+    resolveInit = r;
+  });
+  let isDestroyed = false;
+
+  let cvRouter: CaptureVisionRouter;
+  let cameraEnhancer: CameraEnhancer;
+
+  onMount(() => {
+    (async () => {
+      try {
+        // Create a `CameraEnhancer` instance for camera control and a `CameraView` instance for UI control.
+        const cameraView = await CameraView.createInstance();
+        if (isDestroyed) {
+          throw Error(componentDestroyedErrorMsg);
+        } // Check if component is destroyed after every async
+        cameraEnhancer = await CameraEnhancer.createInstance(cameraView);
+        if (isDestroyed) {
+          throw Error(componentDestroyedErrorMsg);
+        }
+
+        // Get default UI and append it to DOM.
+        cameraViewContainer.append(cameraView.getUIElement());
+
+        // Create a `CaptureVisionRouter` instance and set `CameraEnhancer` instance as its image source.
+        cvRouter = await CaptureVisionRouter.createInstance();
+        if (isDestroyed) {
+          throw Error(componentDestroyedErrorMsg);
+        }
+        cvRouter.setInput(cameraEnhancer);
+
+        // Define a callback for results.
+        cvRouter.addResultReceiver({
+          onDecodedBarcodesReceived: (result) => {
+            if (!result.barcodeResultItems.length) return;
+
+            resultsContainer.textContent = "";
+            console.log(result);
+            for (let item of result.barcodeResultItems) {
+              resultsContainer.textContent += `${item.formatString}: ${item.text}\n\n`;
+            }
+          },
+        });
+
+        // Filter out unchecked and duplicate results.
+        const filter = new MultiFrameResultCrossFilter();
+        // Filter out unchecked barcodes.
+        filter.enableResultCrossVerification("barcode", true);
+        // Filter out duplicate barcodes within 3 seconds.
+        filter.enableResultDeduplication("barcode", true);
+        await cvRouter.addResultFilter(filter);
+        if (isDestroyed) {
+          throw Error(componentDestroyedErrorMsg);
+        }
+
+        //  Open camera and start scanning single barcode.
+        await cameraEnhancer.open();
+        if (isDestroyed) {
+          throw Error(componentDestroyedErrorMsg);
+        }
+        await cvRouter.startCapturing("ReadSingleBarcode");
+        if (isDestroyed) {
+          throw Error(componentDestroyedErrorMsg);
+        }
+      } catch (ex: any) {
+        if ((ex as Error)?.message === componentDestroyedErrorMsg) {
+          console.log(componentDestroyedErrorMsg);
+        } else {
+          let errMsg = ex.message || ex;
+          console.error(errMsg);
+          alert(errMsg);
+        }
+      }
+
+      // distroy function will wait pInit
+      resolveInit!();
+    })();
+
+    // onBeforeUnmount
+    return async () => {
+      isDestroyed = true;
+      try {
+        await pInit;
+        cvRouter?.dispose();
+        cameraEnhancer?.dispose();
+      } catch (_) {}
+    };
+  });
+</script>
+
+<div>
+  <div bind:this={cameraViewContainer} style="width: 100%; height: 70vh; background: #eee;"></div>
+  Results:<br />
+  <div bind:this={resultsContainer} class="results"></div>
+</div>
+
+<style>
+  .results {
+    width: 100%;
+    height: 10vh;
+    overflow: auto;
+    white-space: pre-wrap;
+  }
+</style>
