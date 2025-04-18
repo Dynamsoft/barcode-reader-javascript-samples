6cbc40d89cb5f7eb6b204aa1fb8e9b2c87759317
SHEN <1014628363@qq.com>
2025-04-18 13:57:59 +0800

    add BarcodeScanner samples

---
+++ barcode-scanner-api-samples/scan-single-barcode/angular/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BarcodeScanner } from '@dynamsoft/dynamsoft-barcode-reader-bundle';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular';
+
+  async ngAfterViewInit(): Promise<void> {
+    const config = {
+      license: "YOUR-LICENSE-KEY",
+      container: ".dbs-container",
+      engineResourcePaths: {
+        rootDirectory: "https://npm.scannerproxy.com:802/cdn/@dynamsoft/dynamsoft-barcode-reader-bundle@10.5.1000-dev-20250417174703/dist/",
+      },
+      scannerViewConfig: {
+        cameraEnhancerUIPath: "https://npm.scannerproxy.com:802/cdn/@dynamsoft/dynamsoft-barcode-reader-bundle@10.5.1000-dev-20250417174703/dist/",
+      },
+    }
+    const dbs = new BarcodeScanner(config);
+    dbs.launch().then((result) => {
+      console.log(result);
+    });
+  }
+}
