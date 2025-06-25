bd92d0c8f85ce5a0db1cb224fb8bab70ac227007
SHEN <1014628363@qq.com>
2025-06-25 17:29:48 +0800

    UPDATE v11

--- foundational-api-samples/hello-world/next/components/VideoCapture/VideoCapture.tsx
+++ foundational-api-samples/hello-world/next/components/VideoCapture/VideoCapture.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import "../../dynamsoft.config"; // import side effects. The license, engineResourcePath, so on.
-import { CameraEnhancer, CameraView } from "dynamsoft-camera-enhancer";
-import { CaptureVisionRouter } from "dynamsoft-capture-vision-router";
-import { MultiFrameResultCrossFilter } from "dynamsoft-utility";
+import { CameraEnhancer, CameraView } from "dynamsoft-barcode-reader-bundle";
+import { CaptureVisionRouter } from "dynamsoft-barcode-reader-bundle";
+import { MultiFrameResultCrossFilter } from "dynamsoft-barcode-reader-bundle";
 import "./VideoCapture.css";
 
 const componentDestroyedErrorMsg = "VideoCapture Component Destroyed";
