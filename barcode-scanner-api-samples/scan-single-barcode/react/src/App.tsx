c2bdf09745d3c591eded12ef26ec6ff44f57a1ce
SHEN <1014628363@qq.com>
2025-06-26 16:16:40 +0800

    UPDATE v11

--- barcode-scanner-api-samples/scan-single-barcode/react/src/App.tsx
+++ barcode-scanner-api-samples/scan-single-barcode/react/src/App.tsx
@@ -8,24 +8,31 @@ function App() {
   const hasRun = useRef(false);
 
   useEffect(() => {
-    if(!hasRun.current) {
+    if (!hasRun.current) {
       // Configuration object for initializing the BarcodeScanner instance
       const config = {
         license: "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", // Replace with your Dynamsoft license key
         container: scannerViewRef.current!, // Specify where to render the scanner UI
-  
+
         // Specify the path for the definition file "barcode-scanner.ui.xml" for the scanner view.
         uiPath: "https://cdn.jsdelivr.net/npm/dynamsoft-barcode-reader-bundle@11.0.3000/dist/",
-  
+
+        // showUploadImageButton: true,
+        // showPoweredByDynamsoft: false,
+        // scannerViewConfig: {
+        //   showFlashButton: true,
+        //   cameraSwitchControl: "toggleFrontBack",
+        // },
+
         // Specify custom paths for the engine resources
         engineResourcePaths: {
           rootDirectory: "https://cdn.jsdelivr.net/npm/",
         },
       }
-      
+
       // Create an instance of the BarcodeScanner with the provided configuration
       const barcodeScanner = new BarcodeScanner(config);
-  
+
       // Launch the scanner; once a barcode is detected, display its text in an alert
       barcodeScanner.launch().then((result) => {
         if (result.barcodeResults.length) {
