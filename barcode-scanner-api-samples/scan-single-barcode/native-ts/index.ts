8d1e44dd711223252588f41177ebbe1ec59dcc50
Keillion <keillionv@gmail.com>
2025-09-16 10:40:57 +0800

    demo

--- barcode-scanner-api-samples/scan-single-barcode/native-ts/index.ts
+++ barcode-scanner-api-samples/scan-single-barcode/native-ts/index.ts
@@ -2,7 +2,7 @@ import { BarcodeScanner, BarcodeScannerConfig } from "dynamsoft-barcode-reader-b
 
 // Configuration object for initializing the BarcodeScanner instance
 const config: BarcodeScannerConfig = {
-  license: "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", // Replace with your Dynamsoft license key
+  license: "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAwLWRicl9qc19zYW1wbGVzIiwib3JnYW5pemF0aW9uSUQiOiIyMDAwMDAifQ==", // Replace with your Dynamsoft license key
 
   // Specify where to render the scanner UI
   // If container is not specified, the UI will take up the full screen
