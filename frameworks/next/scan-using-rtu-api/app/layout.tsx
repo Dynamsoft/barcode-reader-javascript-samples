497726de004263f9425de0dee706c902de3d0618
SHEN <1014628363@qq.com>
2026-02-27 16:00:22 +0800

    add rtu sample

---
+++ frameworks/next/scan-using-rtu-api/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Hello World for Next.js - Dynamsoft Barcode Reader Sample",
+  description: "Dynamsoft Barcode Reader in a Next.js Application, helps read barcodes from camera or images.",
+  keywords: "barcodes, camera, images, nextjs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
