bae54c8f2c3ce3b059db96f169233c7bc9d7344e
Justin <Justin@dynamsoft.com>
2026-03-11 14:57:04 +0800

    add google tag & replace demo license

--- frameworks/angular/scan-using-rtu-api/src/app/app.component.ts
+++ frameworks/angular/scan-using-rtu-api/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   async ngAfterViewInit(): Promise<void> {
     // Configuration object for initializing the BarcodeScanner instance
     const config: BarcodeScannerConfig = {
-      license: "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", // Replace with your Dynamsoft license key
+      license: "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAwLWRicl9qc19zYW1wbGVzIiwib3JnYW5pemF0aW9uSUQiOiIyMDAwMDAifQ==", // Replace with your Dynamsoft license key
 
       // Specify where to render the scanner UI
       // If container is not specified, the UI will take up the full screen
